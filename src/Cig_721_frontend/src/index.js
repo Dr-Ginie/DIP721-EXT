7023614ccf7cc3739a70c6834bf2e11772768e3e
Jonathan Green <dev.jonathan.green@gmail.com>
2023-04-03 09:27:36 -0400

    WIP

--- src/Cig_721_frontend/src/index.js
+++ src/Cig_721_frontend/src/index.js
@@ -78,3 +78,90 @@ window.addLayer = async function (number,layer) {
     });
     return actor.addLayer(number,layer)
 }
+
+const colors = [{ value: "red", weight: 100 }, { value: "blue", weight: 100 }, { value: "yellow", weight: 100 }];
+const directions = [{ value: "up", weight: 100 }, { value: "down", weight: 100 }, { value: "left", weight: 100 }, { value: "right", weight: 100 }];
+const animal = [{ value: "wolf", weight: 100 }, { value: "cow", weight: 100 }, { value: "human", weight: 100 }, { value: "mouse", weight: 100 }];
+const layers = [colors, directions, animal];
+
+const DNA_DELIMITER = "-";
+var dnaList = new Set();
+
+const isDnaUnique = (_DnaList = new Set(), _dna = "") => {
+    const _filteredDNA = filterDNAOptions(_dna);
+    return !_DnaList.has(_filteredDNA);
+};
+
+const filterDNAOptions = (_dna) => {
+    const dnaItems = _dna.split(DNA_DELIMITER);
+    const filteredDNA = dnaItems.filter((element) => {
+        const query = /(\?.*$)/;
+        const querystring = query.exec(element);
+        if (!querystring) {
+            return true;
+        }
+        const options = querystring[1].split("&").reduce((r, setting) => {
+            const keyPairs = setting.split("=");
+            return { ...r, [keyPairs[0]]: keyPairs[1] };
+        }, []);
+
+        return options.bypassDNA;
+    });
+
+    return filteredDNA.join(DNA_DELIMITER);
+};
+
+const createDna = (_layers) => {
+    let randNum = [];
+    _layers.forEach((layer) => {
+        var totalWeight = 0;
+        layer.forEach((element) => {
+            totalWeight += element.weight;
+        });
+        // number between 0 - totalWeight
+        let random = Math.floor(Math.random() * totalWeight);
+        for (var i = 0; i < layer.length; i++) {
+            // subtract the current weight from the random weight until we reach a sub zero value.
+            random -= layer[i].weight;
+            if (random < 0) {
+                return randNum.push(
+                    `${layer[i].value}`
+                );
+            }
+        }
+    });
+    return randNum.join(DNA_DELIMITER);
+};
+
+const startCreating = async () => {
+    let layerConfigIndex = 0;
+    let editionCount = 1;
+    let growEditionSizeTo = 20;
+    let failedCount = 0;
+    let abstractedIndexes = [];
+    let uniqueDnaTorrance = 5;
+
+    while (
+        editionCount <= growEditionSizeTo
+    ) {
+        let newDna = createDna(layers);
+        if (isDnaUnique(dnaList, newDna)) {
+            dnaList.add(filterDNAOptions(newDna));
+            editionCount++;
+            abstractedIndexes.shift();
+        } else {
+            console.log("DNA exists!");
+            failedCount++;
+            if (failedCount >= uniqueDnaTorrance) {
+                console.log(
+                    `You need more layers or elements to grow your edition to ${growEditionSizeTo} artworks!`
+                );
+                process.exit();
+            }
+        }
+    }
+    console.log(dnaList.length);
+    console.log(dnaList);
+};
+
+startCreating()
